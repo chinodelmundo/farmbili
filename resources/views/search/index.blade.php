8229e99847140c70b371f04b0ddbf66f4181a9b4
Chino <chinodelmundo@gmail.com>
2017-01-02 00:47:14 +0800

    update readme

--- resources/views/search/index.blade.php
+++ resources/views/search/index.blade.php
@@ -4,27 +4,24 @@
     <div class="container">
         <div class="row"> 
             <div class="col-md-4">
-                <div class = "fixed-position">
-                     @if (Auth::guest())
-                     @else
-                        <div class="panel panel-default">
-                            <div class="panel-heading" data-toggle="collapse" data-target="#navigate">
-                                <a class="panel-title" href="#">
-                                    Site Navigation <span class="caret"></span>
-                                </a>
-                            </div>
-                            
-                            <div id="navigate" class="collapse">
-                                <div class="panel-body">
-                                    @include('layouts.sidebar')
-                                </div>
+                 @if (Auth::guest())
+                 @else
+                    <div class="panel panel-default">
+                        <div class="panel-heading" data-toggle="collapse" data-target="#navigate">
+                            <a class="panel-title" href="#">
+                                Site Navigation <span class="caret"></span>
+                            </a>
+                        </div>
+                        
+                        <div id="navigate" class="collapse">
+                            <div class="panel-body">
+                                @include('layouts.sidebar')
                             </div>
                         </div>
-                    @endif
-                        
-                    @include('search.searchbar')
-
-                </div>
+                    </div>
+                @endif
+                    
+                @include('search.searchbar')
             </div>
             <div class="col-md-8">
                 <div class="container-fluid">
